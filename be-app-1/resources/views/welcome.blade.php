b9e9db3b8284290c0b0f61a8bde42e5ad0af76b7
Emilson C. Cabanatan <133068965+Jettanggg@users.noreply.github.com>
2023-12-15 00:23:27 +0800

    Update welcome.blade.php

--- be-app-1/resources/views/welcome.blade.php
+++ be-app-1/resources/views/welcome.blade.php
@@ -17,14 +17,12 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
-                text-align: center;
                 font-size: 50px;
-                
             }
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-dark-100 dark:bg-dark-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -44,7 +42,7 @@
                     <h6 class="text-center"; >Cabanatan, Emilson C. DIP-IT 3 BLOCK 3</h6>
                 </div>
 
-                <div class="mt-8 bg-dark dark:bg-dark-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
